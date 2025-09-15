d55391bbb8661aec1fcc378d413adf6ea120a301
edymetal <edneymetal@hotmail.com>
2025-09-16 01:01:50 +0200

    Corrige erro 'Cannot read properties of null' em script.js.

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
     const contactLinks = document.getElementById('contact-links');
-    const socialMedia = {
-        'facebook-f': 'https://www.facebook.com/edney.pugleise',
-        'instagram': 'https://www.instagram.com/edneypugliese/',
-        'linkedin-in': 'https://www.linkedin.com/in/edney-vicente-pugliese-machado-35b0688b/',
-        'envelope': 'mailto:edneypugliese@gmail.com'
-    };
 
-    for (const [icon, url] of Object.entries(socialMedia)) {
-        const a = document.createElement('a');
-        a.href = url;
-        a.target = '_blank';
+    if (contactLinks) { // Adiciona esta verificação
+        const socialMedia = {
+            'facebook-f': 'https://www.facebook.com/edney.pugleise',
+            'instagram': 'https://www.instagram.com/edneypugliese/',
+            'linkedin-in': 'https://www.linkedin.com/in/edney-vicente-pugliese-machado-35b0688b/',
+            'envelope': 'mailto:edneypugliese@gmail.com'
+        };
 
-        const i = document.createElement('i');
-        if (icon === 'envelope') {
-            i.className = `fas fa-${icon}`;
-        } else {
-            i.className = `fab fa-${icon}`;
+        for (const [icon, url] of Object.entries(socialMedia)) {
+            const a = document.createElement('a');
+            a.href = url;
+            a.target = '_blank';
+
+            const i = document.createElement('i');
+            if (icon === 'envelope') {
+                i.className = `fas fa-${icon}`;
+            } else {
+                i.className = `fab fa-${icon}`;
+            }
+            
+            a.appendChild(i);
+            contactLinks.appendChild(a);
         }
-        
-        a.appendChild(i);
-        contactLinks.appendChild(a);
     }
 });
