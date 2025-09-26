393f31c078e5e2e68d73a9a7352b72f334f4b3f1
edymetal <edneymetal@hotmail.com>
2025-09-26 21:40:35 +0200

    feat: Reestruturar modal de portfólio e adicionar seção de tecnologia

--- script.js
+++ script.js
@@ -117,12 +117,14 @@ document.addEventListener('DOMContentLoaded', function() {
         const modalProjectImage = document.getElementById('modal-project-image');
         const modalDescription = portfolioModal.querySelector('.modal-description');
         const modalDetails = portfolioModal.querySelector('.modal-details');
+        const modalTechnology = portfolioModal.querySelector('.modal-technology');
         const closeButton = portfolioModal.querySelector('.close-button');
 
         document.querySelectorAll('.portfolio-item').forEach(item => {
             item.addEventListener('click', () => {
                 const descriptionKey = item.getAttribute('data-description');
                 const detailsKey = item.getAttribute('data-details');
+                const technologyKey = item.getAttribute('data-technology'); // Adicionado
                 const titleKey = item.querySelector('.portfolio-title').getAttribute('data-i18n');
                 const imageUrl = item.querySelector('img').src;
 
@@ -130,6 +132,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 modalProjectImage.src = imageUrl;
                 modalDescription.innerHTML = translations[descriptionKey] || '';
                 modalDetails.innerHTML = translations[detailsKey] || '';
+                modalTechnology.innerHTML = translations[technologyKey] || ''; // Adicionado
 
                 portfolioModal.style.display = 'block';
             });
